14d2e5c839809d6d5fd6ebc3ce7a2f0216ca4a6f
Nate <nathan@creativebyways.com>
2022-05-16 16:09:52 +0100

    tollback

--- piclock.py
+++ piclock.py
@@ -76,10 +76,6 @@ time_list = buildTimeList()
 print("Done")
 # text.AddText("Done")
 
-text.AddText("Love You So Much", 1, 1, 12, Id="Quote" )
-text.AddText("Yours Always, Nathan", 10, 88, 10, Id="Ref")
-text.WriteAll()
-
 # creating a bool value which checks if clock is running
 running = True
 
@@ -95,14 +91,12 @@ while running:
 
     # Write text to the screen at selected point, with an Id
     # Nothing will show on the screen
-#    text.UpdateText(f"{quote[2]}\n", 1, 1, 12, Id="Quote" )
-    text.UpdateText(f"{quote[2]}\n", "Quote" )
+    text.UpdateText(f"{quote[2]}\n", 1, 1, 12, Id="Quote" )
     print(f"{quote[2]}\n")
 
     # Add another line of text, at the default location
     # Nothing will show on the screen
-#    text.UpdateText(f"{quote[4]}, {quote[3]}", 10, 88, 10, Id="Ref")
-    text.UpdateText(f"{quote[4]}, {quote[3]}", "Ref")
+    text.UpdateText(f"{quote[4]}, {quote[3]}", 10, 88, 10, Id="Ref")
     print(f"{quote[4]}, {quote[3]}")
 
     # Now display BOTH lines on the screen
@@ -115,7 +109,7 @@ while running:
 
     os.system("clear")
 
-    clock.sleep(60)
+    clock.sleep(15)
 
     
 
